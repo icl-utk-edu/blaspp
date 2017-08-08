a8f5b7be828b9b2c3a7aa6489cc6a6d92302badf
Mark Gates <mgates3@icl.utk.edu>
2017-08-08 12:00:49 -0400

    includes

--- include/blas.hh
+++ include/blas.hh
@@ -10,12 +10,29 @@
 #include "axpy.hh"
 #include "copy.hh"
 #include "dot.hh"
+#include "iamax.hh"
 #include "nrm2.hh"
+#include "rot.hh"
+#include "rotg.hh"
+#include "rotm.hh"
+#include "rotmg.hh"
 #include "scal.hh"
 #include "swap.hh"
 
 // =============================================================================
 // Level 2 BLAS
+
+#include "gemv.hh"
+#include "ger.hh"
+#include "geru.hh"
+#include "hemv.hh"
+#include "her.hh"
+#include "her2.hh"
+#include "symv.hh"
+#include "syr.hh"
+#include "syr2.hh"
+#include "trmv.hh"
+#include "trsv.hh"
 
 // =============================================================================
 // Level 3 BLAS
