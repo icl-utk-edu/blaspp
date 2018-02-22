23981bba196ba52f6debcae88f046cb501c73549
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2018-02-22 15:48:50 -0500

    tester for device blas

---
+++ test/test_device.hh
@@ -0,0 +1,20 @@
+#ifndef TEST_DEVICE_HH
+#define TEST_DEVICE_HH
+
+#include "libtest.hh"
+#include "blas_util.hh"
+#include "test_common.hh"
+
+// -----------------------------------------------------------------------------
+// Level 1 BLAS
+
+// -----------------------------------------------------------------------------
+// Level 2 BLAS
+
+// -----------------------------------------------------------------------------
+// Level 3 BLAS
+void test_gemm_device  ( Params& params, bool run );
+
+// -----------------------------------------------------------------------------
+
+#endif  //  #ifndef TEST_HH
