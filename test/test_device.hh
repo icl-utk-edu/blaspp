6ae7af88e10562494eace3cfd60ee0659df2399c
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2018-05-17 15:45:23 -0400

    add entries, edit the header guards

--- test/test_device.hh
+++ test/test_device.hh
@@ -14,7 +14,18 @@
 // -----------------------------------------------------------------------------
 // Level 3 BLAS
 void test_gemm_device  ( Params& params, bool run );
+void test_trsm_device  ( Params& params, bool run );
+void test_trmm_device  ( Params& params, bool run );
+void test_hemm_device  ( Params& params, bool run );
+void test_symm_device  ( Params& params, bool run );
+void test_herk_device  ( Params& params, bool run );
+void test_syrk_device  ( Params& params, bool run );
+void test_her2k_device  ( Params& params, bool run );
+void test_syr2k_device  ( Params& params, bool run );
+
+void test_batch_gemm_device( Params& params, bool run );
+void test_batch_trsm_device( Params& params, bool run );
 
 // -----------------------------------------------------------------------------
 
-#endif  //  #ifndef TEST_HH
+#endif  //  #ifndef TEST_DEVIC_HH
