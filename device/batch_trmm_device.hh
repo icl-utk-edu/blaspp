1654937d83399d642b6aa7b7ffa59b7536da7fe0
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2018-05-17 15:30:54 -0400

    batch trmm on gpu

---
+++ device/batch_trmm_device.hh
@@ -0,0 +1,139 @@
+#ifndef DEVICE_BATCH_TRMM_HH
+#define DEVICE_BATCH_TRMM_HH
+
+#include "device.hh"
+#include <limits>
+#include <cstring>
+
+namespace blas {
+
+namespace batch{
+// -----------------------------------------------------------------------------
+/// @ingroup trmm
+inline
+void trmm(
+    std::vector<blas::Side> const &side,
+    std::vector<blas::Uplo> const &uplo,
+    std::vector<blas::Op>   const &trans,
+    std::vector<blas::Diag> const &diag,
+    std::vector<int64_t>    const &m, 
+    std::vector<int64_t>    const &n, 
+    std::vector<float >     const &alpha,
+    std::vector<float*>     const &Aarray, std::vector<int64_t> const &ldda,
+    std::vector<float*>     const &Barray, std::vector<int64_t> const &lddb,
+    const size_t batch,                    std::vector<int64_t>       &info, 
+    blas::Queue &queue )
+{
+    blas_error_if( batch < 0 );
+    blas_error_if( !(info.size() == 0 || info.size() == 1 || info.size() == batch) );
+    if(info.size() > 0){
+        // perform error checking
+        blas::batch::trmm_check<float>( side, uplo, trans, diag,  
+                                        m, n, 
+                                        alpha, Aarray, ldda, 
+                                               Barray, lddb, 
+                                        batch, info );
+    }
+
+    throw std::exception();  // not yet available by the vendor
+}
+
+
+// -----------------------------------------------------------------------------
+/// @ingroup trmm
+inline
+void trmm(
+    std::vector<blas::Side> const &side,
+    std::vector<blas::Uplo> const &uplo,
+    std::vector<blas::Op>   const &trans,
+    std::vector<blas::Diag> const &diag,
+    std::vector<int64_t>    const &m, 
+    std::vector<int64_t>    const &n, 
+    std::vector<double >     const &alpha,
+    std::vector<double*>     const &Aarray, std::vector<int64_t> const &ldda,
+    std::vector<double*>     const &Barray, std::vector<int64_t> const &lddb,
+    const size_t batch,                     std::vector<int64_t>       &info, 
+    blas::Queue &queue )
+{
+    blas_error_if( batch < 0 );
+    blas_error_if( !(info.size() == 0 || info.size() == 1 || info.size() == batch) );
+    if(info.size() > 0){
+        // perform error checking
+        blas::batch::trmm_check<double>( side, uplo, trans, diag,  
+                                        m, n, 
+                                        alpha, Aarray, ldda, 
+                                               Barray, lddb, 
+                                        batch, info );
+    }
+
+    throw std::exception();  // not yet available by the vendor
+}
+
+
+// -----------------------------------------------------------------------------
+/// @ingroup trmm
+inline
+void trmm(
+    std::vector<blas::Side> const &side,
+    std::vector<blas::Uplo> const &uplo,
+    std::vector<blas::Op>   const &trans,
+    std::vector<blas::Diag> const &diag,
+    std::vector<int64_t>    const &m, 
+    std::vector<int64_t>    const &n, 
+    std::vector<std::complex<float> >     const &alpha,
+    std::vector<std::complex<float>*>     const &Aarray, std::vector<int64_t> const &ldda,
+    std::vector<std::complex<float>*>     const &Barray, std::vector<int64_t> const &lddb,
+    const size_t batch,                     std::vector<int64_t>       &info, 
+    blas::Queue &queue )
+{
+    blas_error_if( batch < 0 );
+    blas_error_if( !(info.size() == 0 || info.size() == 1 || info.size() == batch) );
+    if(info.size() > 0){
+        // perform error checking
+        blas::batch::trmm_check<std::complex<float>>( side, uplo, trans, diag,  
+                                        m, n, 
+                                        alpha, Aarray, ldda, 
+                                               Barray, lddb, 
+                                        batch, info );
+    }
+
+    throw std::exception();  // not yet available by the vendor
+}
+
+
+// -----------------------------------------------------------------------------
+/// @ingroup trmm
+inline
+void trmm(
+    std::vector<blas::Side> const &side,
+    std::vector<blas::Uplo> const &uplo,
+    std::vector<blas::Op>   const &trans,
+    std::vector<blas::Diag> const &diag,
+    std::vector<int64_t>    const &m, 
+    std::vector<int64_t>    const &n, 
+    std::vector<std::complex<double> >     const &alpha,
+    std::vector<std::complex<double>*>     const &Aarray, std::vector<int64_t> const &ldda,
+    std::vector<std::complex<double>*>     const &Barray, std::vector<int64_t> const &lddb,
+    const size_t batch,                     std::vector<int64_t>       &info, 
+    blas::Queue &queue )
+{
+    blas_error_if( batch < 0 );
+    blas_error_if( !(info.size() == 0 || info.size() == 1 || info.size() == batch) );
+    if(info.size() > 0){
+        // perform error checking
+        blas::batch::trmm_check<std::complex<double>>( side, uplo, trans, diag,  
+                                        m, n, 
+                                        alpha, Aarray, ldda, 
+                                               Barray, lddb, 
+                                        batch, info );
+    }
+
+    throw std::exception();  // not yet available by the vendor
+}
+
+
+}        //  namespace batch
+}        //  namespace blas
+
+#endif        //  #ifndef DEVICE_BATCH_TRMM_HH
+
