0d3eef7e7e8f555ed76c2ff67289ca86ee08e67d
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2018-07-13 09:49:39 -0400

    few edits

--- test/test_batch_gemm_device.cc
+++ test/test_batch_gemm_device.cc
@@ -19,16 +19,16 @@ void test_device_batch_gemm_work( Params& params, bool run )
 
     // get & mark input values
     blas::Layout layout = params.layout.value();
-    blas::Op transA = params.transA.value();
-    blas::Op transB = params.transB.value();
-    scalar_t alpha  = params.alpha.value();
-    scalar_t beta   = params.beta.value();
-    int64_t m       = params.dim.m();
-    int64_t n       = params.dim.n();
-    int64_t k       = params.dim.k();
-    int64_t batch   = params.batch.value();
-    int64_t align   = params.align.value();
-    int64_t verbose = params.verbose.value();
+    blas::Op transA_    = params.transA.value();
+    blas::Op transB_    = params.transB.value();
+    scalar_t alpha_     = params.alpha.value();
+    scalar_t beta_      = params.beta.value();
+    int64_t m_          = params.dim.m();
+    int64_t n_          = params.dim.n();
+    int64_t k_          = params.dim.k();
+    size_t  batch       = params.batch.value();
+    int64_t align       = params.align.value();
+    int64_t verbose     = params.verbose.value();
 
     // mark non-standard output values
     params.ref_time.value();
@@ -38,19 +38,19 @@ void test_device_batch_gemm_work( Params& params, bool run )
         return;
 
     // setup
-    int64_t Am = (transA == Op::NoTrans ? m : k);
-    int64_t An = (transA == Op::NoTrans ? k : m);
-    int64_t Bm = (transB == Op::NoTrans ? k : n);
-    int64_t Bn = (transB == Op::NoTrans ? n : k);
-    int64_t Cm = m;
-    int64_t Cn = n;
+    int64_t Am = (transA_ == Op::NoTrans ? m_ : k_);
+    int64_t An = (transA_ == Op::NoTrans ? k_ : m_);
+    int64_t Bm = (transB_ == Op::NoTrans ? k_ : n_);
+    int64_t Bn = (transB_ == Op::NoTrans ? n_ : k_);
+    int64_t Cm = m_;
+    int64_t Cn = n_;
     
-    int64_t lda = roundup( Am, align );
-    int64_t ldb = roundup( Bm, align );
-    int64_t ldc = roundup( Cm, align );
-    size_t size_A = size_t(lda)*An;
-    size_t size_B = size_t(ldb)*Bn;
-    size_t size_C = size_t(ldc)*Cn;
+    int64_t lda_ = roundup( Am, align );
+    int64_t ldb_ = roundup( Bm, align );
+    int64_t ldc_ = roundup( Cm, align );
+    size_t size_A = size_t(lda_)*An;
+    size_t size_B = size_t(ldb_)*Bn;
+    size_t size_C = size_t(ldc_)*Cn;
     TA* A    = new TA[ batch * size_A ];
     TB* B    = new TB[ batch * size_B ];
     TC* C    = new TC[ batch * size_C ];
@@ -58,58 +58,54 @@ void test_device_batch_gemm_work( Params& params, bool run )
 
     // device specifics 
     blas::Queue queue(0, batch);
-    TA* dA; 
-    TB* dB; 
-    TC* dC;
-
-    dA = blas::device_malloc<TA>( batch * size_A );
-    dB = blas::device_malloc<TB>( batch * size_B );
-    dC = blas::device_malloc<TC>( batch * size_C );
+    TA* dA = blas::device_malloc<TA>( batch * size_A );
+    TB* dB = blas::device_malloc<TB>( batch * size_B );
+    TC* dC = blas::device_malloc<TC>( batch * size_C );
 
     // pointer arrays
     std::vector<TA*>    Aarray( batch );
     std::vector<TB*>    Barray( batch );
     std::vector<TC*>    Carray( batch );
+    std::vector<TC*> Crefarray( batch );
     std::vector<TA*>   dAarray( batch );
     std::vector<TB*>   dBarray( batch );
     std::vector<TC*>   dCarray( batch );
-    std::vector<TC*> Crefarray( batch );
 
-    for(int64_t i = 0; i < batch; i++){
-         Aarray[i]   =  A   + i * size_A;
-         Barray[i]   =  B   + i * size_B;
-         Carray[i]   =  C   + i * size_C;
-        dAarray[i]   = dA   + i * size_A;
-        dBarray[i]   = dB   + i * size_B;
-        dCarray[i]   = dC   + i * size_C;
-        Crefarray[i] = Cref + i * size_C;
+    for(size_t s = 0; s < batch; s++){
+         Aarray[s]   =  A   + s * size_A;
+         Barray[s]   =  B   + s * size_B;
+         Carray[s]   =  C   + s * size_C;
+        Crefarray[s] = Cref + s * size_C;
+        dAarray[s]   = dA   + s * size_A;
+        dBarray[s]   = dB   + s * size_B;
+        dCarray[s]   = dC   + s * size_C;
     }
 
     // info
     std::vector<int64_t> info( batch );
 
     // wrap scalar arguments in std::vector
-    std::vector<blas::Op> vtransA(1, transA);
-    std::vector<blas::Op> vtransB(1, transB);
-    std::vector<int64_t> vm(1, m);
-    std::vector<int64_t> vn(1, n);
-    std::vector<int64_t> vk(1, k);
-    std::vector<int64_t> vldda(1, lda);
-    std::vector<int64_t> vlddb(1, ldb);
-    std::vector<int64_t> vlddc(1, ldc);
-    std::vector<scalar_t> valpha(1, alpha);
-    std::vector<scalar_t> vbeta(1, beta);
+    std::vector<blas::Op> transA(1, transA_);
+    std::vector<blas::Op> transB(1, transB_);
+    std::vector<int64_t>  m(1, m_);
+    std::vector<int64_t>  n(1, n_);
+    std::vector<int64_t>  k(1, k_);
+    std::vector<int64_t>  ldda(1, lda_);
+    std::vector<int64_t>  lddb(1, ldb_);
+    std::vector<int64_t>  lddc(1, ldc_);
+    std::vector<scalar_t> alpha(1, alpha_);
+    std::vector<scalar_t> beta(1, beta_);
 
     int64_t idist = 1;
     int iseed[4] = { 0, 0, 0, 1 };
     lapack_larnv( idist, iseed, batch * size_A, A );
     lapack_larnv( idist, iseed, batch * size_B, B );
     lapack_larnv( idist, iseed, batch * size_C, C );
-    lapack_lacpy( "g", Cm, batch * Cn, C, ldc, Cref, ldc );
+    lapack_lacpy( "g", Cm, batch * Cn, C, ldc_, Cref, ldc_ );
 
-    blas::device_setmatrix(Am, batch * An, A, lda, dA, lda, queue);
-    blas::device_setmatrix(Bm, batch * Bn, B, ldb, dB, ldb, queue);
-    blas::device_setmatrix(Cm, batch * Cn, C, ldc, dC, ldc, queue);
+    blas::device_setmatrix(Am, batch * An, A, lda_, dA, lda_, queue);
+    blas::device_setmatrix(Bm, batch * Bn, B, ldb_, dB, ldb_, queue);
+    blas::device_setmatrix(Cm, batch * Cn, C, ldc_, dC, ldc_, queue);
     queue.sync();
 
     // norms for error check
@@ -118,10 +114,10 @@ void test_device_batch_gemm_work( Params& params, bool run )
     real_t* Bnorm = new real_t[ batch ];
     real_t* Cnorm = new real_t[ batch ];
     
-    for(int64_t i = 0; i < batch; i++){
-        Anorm[i] = lapack_lange( "f", Am, An, Aarray[i], lda, work );
-        Bnorm[i] = lapack_lange( "f", Bm, Bn, Barray[i], ldb, work );
-        Cnorm[i] = lapack_lange( "f", Cm, Cn, Carray[i], ldc, work );
+    for(size_t s = 0; s < batch; s++){
+        Anorm[s] = lapack_lange( "f", Am, An, Aarray[s], lda_, work );
+        Bnorm[s] = lapack_lange( "f", Bm, Bn, Barray[s], ldb_, work );
+        Cnorm[s] = lapack_lange( "f", Cm, Cn, Carray[s], ldc_, work );
     }
 
     // decide error checking mode
@@ -129,27 +125,27 @@ void test_device_batch_gemm_work( Params& params, bool run )
     // run test
     libtest::flush_cache( params.cache.value() );
     double time = get_wtime();
-    blas::batch::gemm( vtransA, vtransB, vm, vn, vk, 
-                       valpha, dAarray, vldda, dBarray, vlddb, vbeta, dCarray, vlddc, 
+    blas::batch::gemm( transA, transB, m, n, k, 
+                       alpha, dAarray, ldda, dBarray, lddb, beta, dCarray, lddc, 
                        batch, info, queue );
     queue.sync();
     time = get_wtime() - time;
 
-    double gflop = batch * Gflop < scalar_t >::gemm( m, n, k );
+    double gflop = batch * Gflop < scalar_t >::gemm( m_, n_, k_ );
     params.time.value()   = time;
     params.gflops.value() = gflop / time;
-    blas::device_getmatrix(Cm, batch * Cn, dC, ldc, C, ldc, queue);
+    blas::device_getmatrix(Cm, batch * Cn, dC, ldc_, C, ldc_, queue);
     queue.sync();
 
     if (params.ref.value() == 'y' || params.check.value() == 'y') {
         // run reference
         libtest::flush_cache( params.cache.value() );
         time = get_wtime();
-        for(int64_t i = 0; i < batch; i++){
+        for(size_t s = 0; s < batch; s++){
             cblas_gemm( cblas_layout_const(layout),
-                        cblas_trans_const(transA),
-                        cblas_trans_const(transB),
-                        m, n, k, alpha, Aarray[i], lda, Barray[i], ldb, beta, Crefarray[i], ldc );
+                        cblas_trans_const(transA_),
+                        cblas_trans_const(transB_),
+                        m_, n_, k_, alpha_, Aarray[s], lda_, Barray[s], ldb_, beta_, Crefarray[s], ldc_ );
         }
         time = get_wtime() - time;
 
@@ -159,9 +155,9 @@ void test_device_batch_gemm_work( Params& params, bool run )
         // check error compared to reference
         real_t err, error = 0;
         bool ok, okay = true;
-        for(int64_t i = 0; i < batch; i++){
-            check_gemm( Cm, Cn, k, alpha, beta, Anorm[i], Bnorm[i], Cnorm[i],
-                        Crefarray[i], ldc, Carray[i], ldc, verbose, &err, &ok );
+        for(size_t s = 0; s < batch; s++){
+            check_gemm( Cm, Cn, k_, alpha_, beta_, Anorm[s], Bnorm[s], Cnorm[s],
+                        Crefarray[s], ldc_, Carray[s], ldc_, verbose, &err, &ok );
             error = max(error, err);
             okay &= ok;
         }
