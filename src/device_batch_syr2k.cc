0566876c1f7f68c462359f699e05d11f6f688108
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2018-07-13 09:44:02 -0400

    add ref impl for device batch syr2k

--- src/device_batch_syr2k.cc
+++ src/device_batch_syr2k.cc
@@ -31,7 +31,26 @@ void blas::batch::syr2k(
                         batch, info );
     }
 
-    throw std::exception();  // not yet available
+    blas::set_device( queue.device() );
+    for(size_t i = 0; i < batch; i++){ 
+        Uplo uplo_   = blas::batch::extract<Uplo>(uplo, i);
+        Op   trans_  = blas::batch::extract<Op>(trans, i);
+        int64_t n_   = blas::batch::extract<int64_t>(n, i);
+        int64_t k_   = blas::batch::extract<int64_t>(k, i);
+        int64_t lda_ = blas::batch::extract<int64_t>(ldda, i);
+        int64_t ldb_ = blas::batch::extract<int64_t>(lddb, i);
+        int64_t ldc_ = blas::batch::extract<int64_t>(lddc, i);
+        float alpha_ = blas::batch::extract<float>(alpha, i);
+        float beta_  = blas::batch::extract<float>(beta, i);
+        float* dA_   = blas::batch::extract<float*>(Aarray, i);
+        float* dB_   = blas::batch::extract<float*>(Barray, i);
+        float* dC_   = blas::batch::extract<float*>(Carray, i);
+        blas::syr2k(
+            Layout::ColMajor, uplo_, trans_, n_, k_, 
+            alpha_, dA_, lda_ , 
+                    dB_, ldb_ , 
+            beta_,  dC_, ldc_ , queue );
+    }
 }
 
 // -----------------------------------------------------------------------------
@@ -62,7 +81,26 @@ void blas::batch::syr2k(
                         batch, info );
     }
 
-    throw std::exception();  // not yet available
+    blas::set_device( queue.device() );
+    for(size_t i = 0; i < batch; i++){ 
+        Uplo uplo_   = blas::batch::extract<Uplo>(uplo, i);
+        Op   trans_  = blas::batch::extract<Op>(trans, i);
+        int64_t n_   = blas::batch::extract<int64_t>(n, i);
+        int64_t k_   = blas::batch::extract<int64_t>(k, i);
+        int64_t lda_ = blas::batch::extract<int64_t>(ldda, i);
+        int64_t ldb_ = blas::batch::extract<int64_t>(lddb, i);
+        int64_t ldc_ = blas::batch::extract<int64_t>(lddc, i);
+        double alpha_ = blas::batch::extract<double>(alpha, i);
+        double beta_  = blas::batch::extract<double>(beta, i);
+        double* dA_   = blas::batch::extract<double*>(Aarray, i);
+        double* dB_   = blas::batch::extract<double*>(Barray, i);
+        double* dC_   = blas::batch::extract<double*>(Carray, i);
+        blas::syr2k(
+            Layout::ColMajor, uplo_, trans_, n_, k_, 
+            alpha_, dA_, lda_ , 
+                    dB_, ldb_ , 
+            beta_,  dC_, ldc_ , queue );
+    }
 }
 
 // -----------------------------------------------------------------------------
@@ -93,7 +131,26 @@ void blas::batch::syr2k(
                         batch, info );
     }
 
-    throw std::exception();  // not yet available
+    blas::set_device( queue.device() );
+    for(size_t i = 0; i < batch; i++){ 
+        Uplo uplo_   = blas::batch::extract<Uplo>(uplo, i);
+        Op   trans_  = blas::batch::extract<Op>(trans, i);
+        int64_t n_   = blas::batch::extract<int64_t>(n, i);
+        int64_t k_   = blas::batch::extract<int64_t>(k, i);
+        int64_t lda_ = blas::batch::extract<int64_t>(ldda, i);
+        int64_t ldb_ = blas::batch::extract<int64_t>(lddb, i);
+        int64_t ldc_ = blas::batch::extract<int64_t>(lddc, i);
+        std::complex<float> alpha_ = blas::batch::extract<std::complex<float>>(alpha, i);
+        std::complex<float> beta_  = blas::batch::extract<std::complex<float>>(beta, i);
+        std::complex<float>* dA_   = blas::batch::extract<std::complex<float>*>(Aarray, i);
+        std::complex<float>* dB_   = blas::batch::extract<std::complex<float>*>(Barray, i);
+        std::complex<float>* dC_   = blas::batch::extract<std::complex<float>*>(Carray, i);
+        blas::syr2k(
+            Layout::ColMajor, uplo_, trans_, n_, k_, 
+            alpha_, dA_, lda_ , 
+                    dB_, ldb_ , 
+            beta_,  dC_, ldc_ , queue );
+    }
 }
 
 // -----------------------------------------------------------------------------
@@ -124,5 +181,24 @@ void blas::batch::syr2k(
                         batch, info );
     }
 
-    throw std::exception();  // not yet available
+    blas::set_device( queue.device() );
+    for(size_t i = 0; i < batch; i++){ 
+        Uplo uplo_   = blas::batch::extract<Uplo>(uplo, i);
+        Op   trans_  = blas::batch::extract<Op>(trans, i);
+        int64_t n_   = blas::batch::extract<int64_t>(n, i);
+        int64_t k_   = blas::batch::extract<int64_t>(k, i);
+        int64_t lda_ = blas::batch::extract<int64_t>(ldda, i);
+        int64_t ldb_ = blas::batch::extract<int64_t>(lddb, i);
+        int64_t ldc_ = blas::batch::extract<int64_t>(lddc, i);
+        std::complex<double> alpha_ = blas::batch::extract<std::complex<double>>(alpha, i);
+        std::complex<double> beta_  = blas::batch::extract<std::complex<double>>(beta, i);
+        std::complex<double>* dA_   = blas::batch::extract<std::complex<double>*>(Aarray, i);
+        std::complex<double>* dB_   = blas::batch::extract<std::complex<double>*>(Barray, i);
+        std::complex<double>* dC_   = blas::batch::extract<std::complex<double>*>(Carray, i);
+        blas::syr2k(
+            Layout::ColMajor, uplo_, trans_, n_, k_, 
+            alpha_, dA_, lda_ , 
+                    dB_, ldb_ , 
+            beta_,  dC_, ldc_ , queue );
+    }
 }
