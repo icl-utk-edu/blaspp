db0b74f1ad906bd509b1715b3f34c412b289ab01
Mark Gates <mgates3@icl.utk.edu>
2018-08-31 08:18:30 -0400

    throw/catch errors more. Sync with changes in libtest.

--- test/test.cc
+++ test/test.cc
@@ -225,86 +225,99 @@ Params::Params():
 // -----------------------------------------------------------------------------
 int main( int argc, char** argv )
 {
+    using libtest::QuitException;
+
     // check that all sections have names
     assert( sizeof(section_names)/sizeof(*section_names) == Section::num_sections );
 
-    // Usage: test routine [params]
-    if (argc < 2 ||
-        strcmp( argv[1], "-h" ) == 0 ||
-        strcmp( argv[1], "--help" ) == 0)
-    {
-        usage( argc, argv, routines, section_names );
-        return 0;
-    }
-
-    // find routine to test
-    const char* routine = argv[1];
-    libtest::test_func_ptr test_routine = find_tester( routine, routines );
-    if (test_routine == nullptr) {
-        fprintf( stderr, "%s%sError: routine %s not found%s\n\n",
-                 libtest::ansi_bold, libtest::ansi_red, routine,
-                 libtest::ansi_normal );
-        usage( argc, argv, routines, section_names );
-        return -1;
-    }
-
-    // mark fields that are used (run=false)
-    Params params;
-    test_routine( params, false );
-
-    // parse parameters after routine name
-    // (prints routine's help and exits for arg = "-h")
-    params.parse( routine, argc-2, argv+2 );
-
-    // print input so running `test [input] > out.txt` documents input
-    printf( "input: %s", argv[0] );
-    for (int i = 1; i < argc; ++i) {
-        printf( " %s", argv[i] );
-    }
-    printf( "\n" );
-
-    // show align column if it has non-default values
-    if (params.align.size() != 1 || params.align.value() != 1) {
-        params.align.width( 5 );
-    }
-
-    // run tests
     int status = 0;
-    auto repeat = params.repeat.value();
-    libtest::DataType last = params.datatype.value();
-    params.header();
-    do {
-        if (params.datatype.value() != last) {
-            last = params.datatype.value();
-            printf( "\n" );
+    try {
+        // print input so running `test [input] > out.txt` documents input
+        printf( "input: %s", argv[0] );
+        for (int i = 1; i < argc; ++i) {
+            printf( " %s", argv[i] );
         }
-        for (int iter = 0; iter < repeat; ++iter) {
-            try {
-                test_routine( params, true );
-            }
-            catch (blas::Error& err) {
-                params.okay.value() = false;
-                printf( "BLAS error: %s\n", err.what() );
-            }
-            catch (...) {
-                // happens for assert_throw failures
-                params.okay.value() = false;
-                printf( "Caught error\n" );
-            }
-            params.print();
-            status += ! params.okay.value();
-            params.reset_output();
-        }
-        if (repeat > 1) {
-            printf( "\n" );
-        }
-    } while( params.next() );
+        printf( "\n" );
 
-    if (status) {
-        printf( "%d tests FAILED.\n", status );
+        // Usage: test routine [params]
+        if (argc < 2 ||
+            strcmp( argv[1], "-h" ) == 0 ||
+            strcmp( argv[1], "--help" ) == 0)
+        {
+            usage( argc, argv, routines, section_names );
+            throw QuitException();
+        }
+
+        // find routine to test
+        const char* routine = argv[1];
+        libtest::test_func_ptr test_routine = find_tester( routine, routines );
+        if (test_routine == nullptr) {
+            usage( argc, argv, routines, section_names );
+            throw std::runtime_error(
+                std::string("routine ") + routine + " not found" );
+        }
+
+        // mark fields that are used (run=false)
+        Params params;
+        test_routine( params, false );
+
+        // parse parameters after routine name
+        try {
+            params.parse( routine, argc-2, argv+2 );
+        }
+        catch (const std::exception& ex) {
+            params.help( routine );
+            throw;
+        }
+
+        // show align column if it has non-default values
+        if (params.align.size() != 1 || params.align.value() != 1) {
+            params.align.width( 5 );
+        }
+
+        // run tests
+        int repeat = params.repeat.value();
+        libtest::DataType last = params.datatype.value();
+        params.header();
+        do {
+            if (params.datatype.value() != last) {
+                last = params.datatype.value();
+                printf( "\n" );
+            }
+            for (int iter = 0; iter < repeat; ++iter) {
+                try {
+                    test_routine( params, true );
+                }
+                catch (const std::exception& ex) {
+                    fprintf( stderr, "%s%sError: %s%s\n",
+                             ansi_bold, ansi_red, ex.what(), ansi_normal );
+                    params.okay.value() = false;
+                }
+
+                params.print();
+                status += ! params.okay.value();
+                params.reset_output();
+            }
+            if (repeat > 1) {
+                printf( "\n" );
+            }
+        } while( params.next() );
+
+        if (status) {
+            printf( "%d tests FAILED.\n", status );
+        }
+        else {
+            printf( "All tests passed.\n" );
+        }
     }
-    else {
-        printf( "All tests passed.\n" );
+    catch (const QuitException& ex) {
+        // pass: no error to print
     }
+    catch (const std::exception& ex) {
+        fprintf( stderr, "\n%s%sError: %s%s\n",
+                 ansi_bold, ansi_red, ex.what(), ansi_normal );
+        status = -1;
+    }
+
     return status;
 }
