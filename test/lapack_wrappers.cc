f4d3fe709b63b7cb696cbb78b736ab32b68fba47
Mark Gates <mgates3@icl.utk.edu>
2020-02-29 23:04:40 -0500

    Copyright boilerplate

--- test/lapack_wrappers.cc
+++ test/lapack_wrappers.cc
@@ -1,3 +1,8 @@
+// Copyright (c) 2017-2020, University of Tennessee. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the BSD 3-Clause license. See the accompanying LICENSE file.
+
 // Moved from lapack_wrappers.hh for ESSL compatability
 
 // get BLAS_FORTRAN_NAME and blas_int
