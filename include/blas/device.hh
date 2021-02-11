7c4be171c65f65dc7a3a5847804faf3ab7bcd6e3
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2021-02-11 14:33:53 -0500

    initial work for sycl/oneapi: device and memory functions

--- include/blas/device.hh
+++ include/blas/device.hh
@@ -19,18 +19,30 @@
     #endif
     #include <hip/hip_runtime.h>
     #include <rocblas.h>
+#elif defined(BLAS_HAVE_ONEMKL)
+#ifdef __GNUC__
+#include <CL/sycl/detail/cl.h> // for CL version
+#endif
+#include <CL/sycl.hpp>
+#include <oneapi/mkl.hpp>
 #endif
 
 namespace blas {
 
 // -----------------------------------------------------------------------------
 // types
+#ifdef BLAS_HAVE_ONEMKL
+typedef cl::sycl::device Device;
+#else
 typedef int Device;
+#endif
 
 #ifdef BLAS_HAVE_CUBLAS
     typedef int                  device_blas_int;
 #elif defined(BLAS_HAVE_ROCBLAS)
     typedef int                  device_blas_int;
+#elif defined(BLAS_HAVE_ONEMKL)
+    typedef std::int64_t         device_blas_int;
 #else
     typedef int                  device_blas_int;
 #endif
@@ -71,6 +83,14 @@ enum class MemcpyKind : device_blas_int {
             default: throw blas::Error( "unknown memcpy direction" );
         }
     }
+#elif defined(BLAS_HAVE_ONEMKL)
+    /// @return the corresponding sycl memcpy kind constant
+    /*
+     * the memcpy method in the sycl::queue class does not accept
+     * a direction (i.e. always operates in default mode).
+     * for interface compatibility with cuda/hip, return a default integer
+    */
+    inline int64_t memcpy2sycl( MemcpyKind kind ) { return 0; }
 #endif
 
 // -----------------------------------------------------------------------------
@@ -107,6 +127,9 @@ public:
     #elif defined(BLAS_HAVE_ROCBLAS)
         hipStream_t    stream() const { return *current_stream_; }
         rocblas_handle handle() const { return handle_; }
+    #elif defined(BLAS_HAVE_ONEMKL)
+        cl::sycl::queue  stream()     const { return  default_stream_; }
+        cl::sycl::queue* stream_ptr() const { return &default_stream_; }
     #endif
 
 private:
@@ -158,6 +181,11 @@ private:
 
         hipEvent_t   default_event_;
         hipEvent_t   parallel_events_[DEV_QUEUE_FORK_SIZE];
+    #elif defined(BLAS_HAVE_ONMKL)
+        // default sycl queue for this blas queue
+        cl::sycl::queue  default_stream_;
+
+        cl::sycl::event  default_event_;
     #else
         // pointer to current stream (default or fork mode)
         void** current_stream_;
@@ -228,22 +256,59 @@ inline const char* device_error_string( rocblas_status error )
 #elif defined(BLAS_ERROR_ASSERT)
 
     // blaspp aborts on device errors
+    #if defined(BLAS_HAVE_ONEMKL)
+    #define blas_dev_call( error ) \
+        try { \
+            error; \
+        } \
+        catch (cl::sycl::exception const& e) { \
+            blas::internal::abort_if( true, __func__, \
+                                      "%s", e.what() ); \
+        } \
+        catch (std::exception const& e) { \
+            blas::internal::abort_if( true, __func__, \
+                                      "%s", e.what() ); \
+        } \
+        catch (const char* msg) { \
+            blas::internal::abort_if( true, __func__, \
+                                      "%s", msg ); \
+        }
+    #else
     #define blas_dev_call( error ) \
         do { \
             auto e = error; \
             blas::internal::abort_if( blas::is_device_error(e), __func__, \
                                       "%s", blas::device_error_string(e) ); \
         } while(0)
+    #endif
 
 #else
 
     // blaspp throws device errors (default)
+    #if defined(BLAS_HAVE_ONEMKL)
+    try { \
+            error; \
+        } \
+        catch (cl::sycl::exception const& e) { \
+            blas::internal::throw_if( true, \
+                                      e.what(), __func__ ); \
+        } \
+        catch (std::exception const& e) { \
+            blas::internal::throw_if( true, \
+                                      e.what(), __func__ ); \
+        } \
+        catch (const char* msg) { \
+            blas::internal::throw_if( true, \
+                                      msg, __func__ ); \
+        }
+    #else
     #define blas_dev_call( error ) \
         do { \
             auto e = error; \
             blas::internal::throw_if( blas::is_device_error(e), \
                                       blas::device_error_string(e), __func__ ); \
         } while(0)
+    #endif
 
 #endif
 
@@ -276,7 +341,38 @@ T* device_malloc(
     #elif defined(BLAS_HAVE_ROCBLAS)
         blas_dev_call(
             hipMalloc( (void**)&ptr, nelements * sizeof(T) ) );
+
+    #elif defined(BLAS_HAVE_ONEMKL)
+        // SYCK requires a device or queue to malloc
+        throw blas::Error( "unsupported function for sycl backend", __func__ );
+    #endif
+    return ptr;
+}
+
+//------------------------------------------------------------------------------
+/// @return a device pointer to an allocated memory space on specific device
+template <typename T>
+T* device_malloc(
+    blas::Device device, int64_t nelements)
+{
+    T* ptr = nullptr;
+    #ifdef BLAS_HAVE_CUBLAS
+        blas::set_device( device );
+        blas_dev_call(
+                cudaMalloc( (void**)&ptr, nelements * sizeof(T) ) );
+
+    #elif defined(BLAS_HAVE_ROCBLAS)
+        blas::set_device( device );
+        blas_dev_call(
+                hipMalloc( (void**)&ptr, nelements * sizeof(T) ) );
+
+    #ifdef BLAS_HAVE_ONEMKL
+        cl::sycl::queue tmp_queue( device );
+        blas_dev_call(
+            ptr = (T*)cl::sycl::malloc_device(nelements*sizeof(T), tmp_queue) );
+
     #else
+
         throw blas::Error( "device BLAS not available", __func__ );
     #endif
     return ptr;
@@ -296,6 +392,13 @@ T* device_malloc_pinned(
     #elif defined(BLAS_HAVE_ROCBLAS)
         blas_dev_call(
             hipHostMalloc( (void**)&ptr, nelements * sizeof(T) ) );
+
+    #elif defined(BLAS_HAVE_ONEMKL)
+        /*
+         * disable for now on intel gpus
+        */
+        throw blas::Error( "unsupported function for sycl backend", __func__ );
+
     #else
         throw blas::Error( "device BLAS not available", __func__ );
     #endif
@@ -328,6 +431,27 @@ void device_setmatrix(
                 m_, n_, sizeof(T),
                 host_ptr, ldh_,
                 dev_ptr,  ldd_, queue.stream() ) );
+
+    #elif defined(BLAS_HAVE_ONEMKL)
+        if( ldh_ == m_ && ldd_ == m_ ) {
+            /* one memcpy */
+            blas_dev_call(
+                (queue.stream_ptr())->memcpy(dev_ptr, host_ptr, m_*n_*sizeof(T));
+            );
+        }
+        else {
+            /* will have to do several mem-copies
+             * sycl does not support set/get matrix
+            */
+            T *dptr=NULL, *hptr=NULL;
+            for(int64_t ic = 0; ic < n_; ++ic) {
+                dptr = dev_ptr[ic*ldd_];
+                hptr = host_ptr[ic*ldh_];
+                blas_dev_call(
+                    (queue.stream_ptr())->memcpy(dptr, hptr, m_*sizeof(T)) );
+            }
+        }
+
     #else
         throw blas::Error( "device BLAS not available", __func__ );
         blas_unused( m_ );
@@ -363,6 +487,26 @@ void device_getmatrix(
                 m_, n_, sizeof(T),
                 dev_ptr,  ldd_,
                 host_ptr, ldh_, queue.stream() ) );
+
+    #elif defined(BLAS_HAVE_ONEMKL)
+        if( ldh_ == m_ && ldd_ == m_ ) {
+            /* one memcpy */
+            blas_dev_call(
+                (queue.stream_ptr())->memcpy(host_ptr, dev_ptr, m_*n_*sizeof(T)) );
+        }
+        else {
+            /* will have to do several mem-copies
+             * sycl does not support set/get matrix
+            */
+            T *dptr=NULL, *hptr=NULL;
+            for(int64_t ic = 0; ic < n_; ++ic) {
+                dptr = dev_ptr[ic*ldd_];
+                hptr = host_ptr[ic*ldh_];
+                blas_dev_call(
+                    (queue.stream_ptr())->memcpy(hptr, dptr, m_*sizeof(T)) );
+            }
+        }
+
     #else
         throw blas::Error( "device BLAS not available", __func__ );
         blas_unused( m_ );
@@ -397,6 +541,23 @@ void device_setvector(
                 n_, sizeof(T),
                 host_ptr, inch_,
                 dev_ptr,  incd_, queue.stream() ) );
+
+    #elif defined(BLAS_HAVE_ONEMKL)
+        if( inch_ == incd_ ) {
+            /* this could be slow if inc >> n */
+            blas_dev_call(
+                (queue.stream_ptr())->memcpy(host_ptr, dev_ptr, n_*inch_*sizeof(T)) );
+        }
+        else {
+            T *hptr=NULL, *dptr=NULL;
+            for(int64_t ie = 0; ie < n_; ++ie) {
+                hptr = host_ptr + ie * inch;
+                pptr = dev_ptr  + ie * incd;
+                blas_dev_call(
+                    (queue.stream_ptr())->memcpy(hptr, dptr, sizeof(T)) );
+            }
+        }
+
     #else
         throw blas::Error( "device BLAS not available", __func__ );
         blas_unused( n_ );
@@ -430,6 +591,23 @@ void device_getvector(
                 n_, sizeof(T),
                 dev_ptr,  incd_,
                 host_ptr, inch_, queue.stream() ) );
+
+    #elif defined(BLAS_HAVE_ONEMKL)
+        if( inch_ == incd_ ) {
+            /* this could be slow if inc >> n */
+            blas_dev_call(
+                (queue.stream_ptr())->memcpy(host_ptr, dev_ptr, n_*inch_*sizeof(T)) );
+        }
+        else {
+            T *hptr=NULL, *dptr=NULL;
+            for(int64_t ie = 0; ie < n_; ++ie) {
+                hptr = host_ptr + ie * inch;
+                pptr = dev_ptr  + ie * incd;
+                blas_dev_call(
+                    (queue.stream_ptr())->memcpy(hptr, dptr, sizeof(T)) );
+            }
+        }
+
     #else
         throw blas::Error( "device BLAS not available", __func__ );
         blas_unused( n_ );
@@ -456,6 +634,11 @@ void device_memset(
             hipMemsetAsync(
                 ptr, value,
                 nelements * sizeof(T), queue.stream() ) );
+
+    #elif defined(BLAS_HAVE_ONEMKL)
+        blas_dev_call(
+            (queue.stream_ptr())->memset(ptr, value, nelements * sizeof(T)) );
+
     #else
         throw blas::Error( "device BLAS not available", __func__ );
     #endif
@@ -480,6 +663,11 @@ void device_memcpy(
             hipMemcpyAsync(
                 dev_ptr, host_ptr, sizeof(T)*nelements,
                 memcpy2hip(kind), queue.stream() ) );
+
+    #elif defined(BLAS_HAVE_ONEMKL)
+        blas_dev_call(
+            (queue.stream_ptr())->memcpy(dev_ptr, host_ptr, sizeof(T)*nelements) );
+
     #else
         throw blas::Error( "device BLAS not available", __func__ );
     #endif
@@ -518,6 +706,10 @@ void device_memcpy_2d(
                  dev_ptr, sizeof(T)* dev_pitch,
                 host_ptr, sizeof(T)*host_pitch,
                 sizeof(T)*width, height, memcpy2hip(kind), queue.stream() ) );
+
+    #elif defined(BLAS_HAVE_ONEMKL)
+        throw blas::Error( "unsupported function for sycl backend", __func__ );
+
     #else
         throw blas::Error( "device BLAS not available", __func__ );
     #endif
