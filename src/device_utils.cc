64850857cf3770e41270548b3fdf100702110998
Ahmad Abdelfattah <ahmad@icl.utk.edu>
2021-02-22 10:23:03 -0500

    new function: enumerate_devices, plus some minor fixes

--- src/device_utils.cc
+++ src/device_utils.cc
@@ -33,24 +33,24 @@ void set_device(blas::Device device)
 // get current device
 void get_device(blas::Device *device)
 {
-    device_blas_int dev = -1;
     #ifdef BLAS_HAVE_CUBLAS
+        device_blas_int dev = -1;
         blas_dev_call(
             cudaGetDevice(&dev) );
+        (*device) = (blas::Device)dev;
 
     #elif defined(BLAS_HAVE_ROCBLAS)
+        device_blas_int dev = -1;
         blas_dev_call(
             hipGetDevice(&dev) );
+        (*device) = (blas::Device)dev;
 
     #elif defined(BLAS_HAVE_ONEMKL)
         throw blas::Error( "unsupported function for sycl backend", __func__ );
-        blas_unused( dev );
 
     #else
         throw blas::Error( "device BLAS not available", __func__ );
-        blas_unused( dev );
     #endif
-    (*device) = (blas::Device)dev;
 }
 
 // -----------------------------------------------------------------------------
@@ -70,8 +70,7 @@ device_blas_int get_device_count()
             blas_dev_call( err );
 
     #elif defined(BLAS_HAVE_ONEMKL)
-
-    auto platforms = platform::get_platforms();
+    auto platforms = sycl::platform::get_platforms();
     for (auto &platform : platforms) {
         auto devices = platform.get_devices();
         for (auto &device : devices ) {
@@ -83,6 +82,45 @@ device_blas_int get_device_count()
         // return dev_count = 0
     #endif
 
+    return dev_count;
+}
+
+// -----------------------------------------------------------------------------
+// @return number of GPU devices
+device_blas_int enumerate_devices(std::vector<blas::Device> &devices)
+{
+    device_blas_int dev_count = get_device_count();
+
+    if( devices.size() != (size_t)dev_count ) {
+        devices.clear();
+        devices.reserve( dev_count );
+    }
+
+    #if defined(BLAS_HAVE_CUBLAS) || defined(BLAS_HAVE_ROCBLAS)
+    for( auto i = 0; i < dev_count; i++) {
+        devices[ i ] = (blas::Device) i;
+    }
+
+    #elif defined(BLAS_HAVE_ONEMKL)
+    auto platforms = sycl::platform::get_platforms();
+    for (auto &platform : platforms) {
+        auto all_devices = platform.get_devices();
+        for (auto &idevice : all_devices ) {
+            if ( idevice.is_gpu() ) {
+                devices.push_back( idevice );
+            }
+        }
+    }
+
+    // must remove the if statement below in production mode
+    if( devices.size() == 0 ) {
+        devices.push_back( sycl::device(sycl::gpu_selector()) );
+        dev_count = 1;
+    }
+    #else
+    // get_device_count returns 0,
+    // and so devices will become an empty vector
+    #endif
     return dev_count;
 }
 
