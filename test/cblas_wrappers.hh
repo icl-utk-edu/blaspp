322edd6327d4ce98edaac936bdfc66e6f8d86fad
Asim YarKhan <yarkhan@icl.utk.edu>
2022-03-15 13:34:57 +0000

    Minor changes.  For OneMKL only need to include mkl_cblas.h (not mkl_blas.h).

--- test/cblas_wrappers.hh
+++ test/cblas_wrappers.hh
@@ -13,14 +13,13 @@
         #define MKL_ILP64
     #endif
     #include <CL/sycl/detail/cl.h>  // For CL version
-    #include <mkl.hpp>
+    #include <mkl_cblas.h>
 
 #elif defined(BLAS_HAVE_MKL)
     #if defined(BLAS_ILP64) && ! defined(MKL_ILP64)
         #define MKL_ILP64
     #endif
     #include <mkl_cblas.h>
-
 
 #elif defined(BLAS_HAVE_ESSL)
     #if defined(BLAS_ILP64) && ! defined(_ESV6464)
