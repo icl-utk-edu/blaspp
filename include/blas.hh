198d0555075e83ea6f79e4df7c42af4ca9fa92f7
Mark Gates <mgates3@icl.utk.edu>
2022-05-31 03:04:48 -0400

    Version 2022.05.00

--- include/blas.hh
+++ include/blas.hh
@@ -9,8 +9,8 @@
 #include "blas/defines.h"
 
 // Version is updated by make_release.py; DO NOT EDIT.
-// Version 2021.04.01
-#define BLASPP_VERSION 20210401
+// Version 2022.05.00
+#define BLASPP_VERSION 20220500
 
 namespace blas {
 
