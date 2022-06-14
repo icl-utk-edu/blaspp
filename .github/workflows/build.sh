3510cfc21087c87ef075a86610838d5a42c0a26b
Gerald Ragghianti <ragghianti@icl.utk.edu>
2022-06-14 15:59:23 -0400

    Testing CI

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -17,23 +17,24 @@ module load intel-mkl
 echo "======================================== load CUDA or ROCm"
 # Load CUDA.
 if [ "${gpu}" = "nvidia" ]; then
-    which nvcc
-    nvcc --version
     # Load CUDA. 
     export CUDA_HOME=/usr/local/cuda/
+    export PATH=${PATH}:${CUDA_HOME}/bin
     export CPATH=${CPATH}:${CUDA_HOME}/include
     export LIBRARY_PATH=${LIBRARY_PATH}:${CUDA_HOME}/lib64
+    which nvcc
+    nvcc --version
 fi
 
 # Load HIP.
 if [ "${gpu}" = "amd" ]; then
-    which hipcc
-    hipcc --version
     # Load ROCm/HIP.
     export PATH=${PATH}:/opt/rocm/bin
     export CPATH=${CPATH}:/opt/rocm/include
     export LIBRARY_PATH=${LIBRARY_PATH}:/opt/rocm/lib:/opt/rocm/lib64
     export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/rocm/lib:/opt/rocm/lib64
+    which hipcc
+    hipcc --version
 fi
 
 echo "======================================== verify dependencies"
