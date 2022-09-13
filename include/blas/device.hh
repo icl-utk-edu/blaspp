9c6dcad97d30a55f48b5c5629220ee8401dac8e0
Mark Gates <mgates3@icl.utk.edu>
2022-09-13 03:08:51 -0400

    update include path for ROCm 5.2; fixes bitbucket issue #20

--- include/blas/device.hh
+++ include/blas/device.hh
@@ -21,7 +21,13 @@
     #endif
 
     #include <hip/hip_runtime.h>
-    #include <rocblas.h>
+
+    // Headers moved in ROCm 5.2
+    #if HIP_VERSION >= 50200000
+        #include <rocblas/rocblas.h>
+    #else
+        #include <rocblas.h>
+    #endif
 
     // If we defined __HIP_PLATFORM_HCC__, undef it.
     #ifdef BLAS_HIP_PLATFORM_HCC
